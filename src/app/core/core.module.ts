204803298fd0cd9251692d440263a904f3e23c03
Coder Base <coderbaseit@gmail.com>
2020-06-23 14:02:44 +0100

    RouterModule et simple route config

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UiModule } from '../ui/ui.module';
+import { NgModule } from '@angular/core';
 import { IconModule } from '../icon/icon.module';
+import { TextModule } from '../text/text.module';
+import { UiModule } from '../ui/ui.module';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { TextModule } from '../text/text.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -16,7 +17,8 @@ import { TextModule } from '../text/text.module';
     CommonModule,
     UiModule,
     IconModule,
-    TextModule
+    TextModule,
+    RouterModule
   ]
 })
 export class CoreModule { }
